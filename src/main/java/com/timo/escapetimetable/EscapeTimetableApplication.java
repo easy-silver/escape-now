551b6dba53fc63c2db1453af0203051ef7d4cec7
Timo <developer.timo@gmail.com>
2021-11-02 21:19:08 +0900

    Initial commit

---
+++ src/main/java/com/timo/escapetimetable/EscapeTimetableApplication.java
@@ -0,0 +1,13 @@
+package com.timo.escapetimetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapeTimetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscapeTimetableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timo/escapetimetable/EscapeTimetableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timo.escapetimetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeTimetableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
