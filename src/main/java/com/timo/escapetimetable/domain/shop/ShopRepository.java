b1724305da677b33db8d0b5d0a7a3eb2119c1dd3
Timo <developer.timo@gmail.com>
2021-11-02 23:49:11 +0900

    지역별 매장 목록 조회 추가

--- src/main/java/com/timo/escapetimetable/domain/shop/ShopRepository.java
+++ src/main/java/com/timo/escapetimetable/domain/shop/ShopRepository.java
@@ -1,7 +1,25 @@
 package com.timo.escapetimetable.domain.shop;
 
+import com.timo.escapetimetable.domain.AreaCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ShopRepository extends JpaRepository<Shop, Long> {
 
+    /**
+     * 지역 코드별 매장 목록 조회
+     *
+     * @param areaCode
+     * @return List<Shop>
+     */
+    @Query("SELECT s " +
+            "FROM Shop s " +
+            "JOIN Branch b " +
+            "ON s.id = b.shop.id " +
+            "WHERE b.areaCode = :areaCode " +
+            "GROUP BY s.id")
+    List<Shop> findAllByAreaCode(@Param("areaCode") AreaCode areaCode);
 }
---
+++ src/test/java/com/timo/escapetimetable/domain/shop/ShopRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.timo.escapetimetable.domain.shop;
+
+import com.timo.escapetimetable.domain.AreaCode;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ShopRepositoryTest {
+
+    @Autowired ShopRepository repository;
+
+    @Test
+    void 전체_매장_조회() {
+        //when
+        List<Shop> result = repository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    void 단일_매장_조회() {
+        //given
+        Long shopId = 1L;
+
+        //when
+        Shop findShop = repository.findById(shopId).get();
+
+        //then
+        assertThat(findShop.getName()).isEqualTo("넥스트에디션");
+    }
+    
+    @Test
+    void 지역코드별_매장_조회() {
+        //given
+        //AreaCode areaCode = AreaCode.GN;
+        AreaCode areaCode = AreaCode.HD;
+
+        //when
+        List<Shop> result = repository.findAllByAreaCode(areaCode);
+
+        //then
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+}
